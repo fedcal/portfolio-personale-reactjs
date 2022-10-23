199ff785b00408415bf786282faabfc0d084a2c5
fedcal <fedcal01@gmail.com>
2022-10-23 11:10:53 +0200

    servizi helmet

--- src/routes/Servizi.js
+++ src/routes/Servizi.js
@@ -3,10 +3,27 @@ import Footer from '../components/Footer/Footer';
 import HeroImg2 from '../components/Hero/HeroImg2';
 import Navbar from '../components/Navbar/Navbar';
 import ServiziCard from '../components/Servizi/ServiziCard';
+import {Helmet} from "react-helmet";
 
 function Servizi() {
   return (
     <div>
+      <Helmet>
+        <title>Federico Calò - Progetti sviluppati</title>
+        <meta name='keywords' content='siti web, servizi, sviluppo software, analisi tecnica, Studio SEO, ottimizzazione, SEO, design, gestionali, Data mining, analisi dei dait, documentazione dettagliata, modelli di regressione, Data Visualitation'/>
+        <meta name='description' content='Servizi offerti da Fedeirco Calò per la creazione di siti web, gestionali o software per la propria azienda. Analisi dei dati e documentazione tecnica funzionale.'/>
+        <meta name='robots' content='index,follow'/>
+        <meta name='language' content='IT'/>
+        <meta name='topic' content='Programmazione e Sviluppo'/>
+        <meta name='author' content='Federico Calò, fedcal01@gmail.com'/>
+        <meta name='og:title' content='Federico Calò - Progetti sviluppati'/>
+        <meta name='og:description' content='Raccolta di progetti sviluppati in diverse tecnologie, le principali sono: Java, ReactJs, SpringBoot, HTML, CSS, Helmet.'/>
+        <meta name='og:email' content='fedcal01@gmail.com'/>
+        <meta name='og:phone_number' content='+39 3332673965'/>
+        <meta name="apple-mobile-web-app-title" content="Federico Calò - Progetti sviluppati"/> 
+        <meta name='apple-mobile-web-app-capable' content='yes'/>
+        <meta name='apple-touch-fullscreen' content='yes'/>
+      </Helmet>
         <Navbar/>
         <HeroImg2 heading="SERVIZI"text="Ecco in cosa posso esserti d'aiuto"/>
         <ServiziCard/>
