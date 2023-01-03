57443fef7917a192ecebc5de3b45c85dc4aca127
fedcal <fedcal01@gmail.com>
2023-01-03 22:43:11 +0100

    lazy home

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React, {lazy} from "react";
 import './index.css';
-
-import About from "./routes/About";
-import Contatti from "./routes/Contact";
-import Progetti from "./routes/Project";
-import Servizi from "./routes/Servizi";
 import { Routes, Route } from "react-router-dom";
-import Blog from "./routes/Blog";
-import Reti01 from "./components/Blog/articoli/Reti/Reti01/Reti01";
-import Libri01 from "./components/Blog/articoli/Libri/Libri01/Libri01";
-import IUM1 from "./components/Blog/articoli/IUM/IUM1/IUM1";
-import Reti02 from "./components/Blog/articoli/Reti/Reti02/Reti02";
-import IUM2 from "./components/Blog/articoli/IUM/IUM2/IUM2";
-import Intro from "./components/Blog/articoli/StudyCase/GestionaleAssociativo/Intro";
-import Er from "./components/Blog/articoli/StudyCase/GestionaleAssociativo/2_ER/Er";
-import IUM3 from "./components/Blog/articoli/IUM/IUM3/IUM3";
-import ICON1 from "./components/Blog/articoli/ICON/ICON1/ICON1";
-import ICON2 from "./components/Blog/articoli/ICON/ICON2/ICON2";
-import ICON3 from "./components/Blog/articoli/ICON/ICON3/ICON3";
 
-const LazyHome =lazy(()=>import('./routes/Home'))
+import Home from "./routes/Home";
+
+const About = lazy(()=>import("./routes/About"));
+const Contatti = lazy(()=>import("./routes/Contact"));
+const Progetti = lazy(()=>import("./routes/Project"));
+const Servizi = lazy(()=>import("./routes/Servizi"));
+const Blog = lazy(()=>import("./routes/Blog"));
+const Reti01 = lazy(()=>import("./components/Blog/articoli/Reti/Reti01/Reti01"));
+const Libri01 = lazy(()=>import("./components/Blog/articoli/Libri/Libri01/Libri01"));
+const IUM1 = lazy(()=>import("./components/Blog/articoli/IUM/IUM1/IUM1"));
+const Reti02 = lazy(()=>import("./components/Blog/articoli/Reti/Reti02/Reti02"));
+const IUM2 = lazy(()=>import("./components/Blog/articoli/IUM/IUM2/IUM2"));
+const Intro = lazy(()=>import("./components/Blog/articoli/StudyCase/GestionaleAssociativo/Intro"));
+const Er =lazy(()=>import( "./components/Blog/articoli/StudyCase/GestionaleAssociativo/2_ER/Er"));
+const IUM3 = lazy(()=>import("./components/Blog/articoli/IUM/IUM3/IUM3"));
+const ICON1 = lazy(()=>import("./components/Blog/articoli/ICON/ICON1/ICON1"));
+const ICON2 = lazy(()=>import("./components/Blog/articoli/ICON/ICON2/ICON2"));
+const ICON3 = lazy(()=>import("./components/Blog/articoli/ICON/ICON3/ICON3"));
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/'element={<LazyHome/>}/>
+        <Route path='/'element={<Home/>}/>
         <Route path='/progetti'element={<Progetti/>}/>
         <Route path='/presentazioni'element={<About/>}/>
         <Route path='/servizi'element={<Servizi/>}/>
