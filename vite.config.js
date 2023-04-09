e0fd32aea7bc207a6108bf5792c9d383e8070c54
fedcal <fedcal01@gmail.com>
2023-04-09 10:22:04 +0200

    add vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginTransformCssModules from 'vite-plugin-transform-css-modules';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginTransformCssModules(),
+    babel()
+  ],
   build: {
     chunkSizeWarningLimit: 1600,
   },
